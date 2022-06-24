35dad8e0a96f5e2e5e770aa7040157374fe0b4b4
Taichi Sato <okms310gmail.com>
2022-06-24 16:42:18 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/teach310/genta
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Hello World\n")
+	// if err := run(); err != nil {
+	// 	log.Fatalln(err)
+	// }
+}
