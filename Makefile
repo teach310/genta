e264dcf2dc0e1bb0ae1b72495051dba1d8454483
Taichi Sato <okms310gmail.com>
2022-07-27 11:29:15 +0900

    feat optionsを渡せるようにする。
    オプションは「--genta_opt=オプション名=値」という形で渡す。
    =が2つある都合上値がなにかは明示したほうが良いので、=は必須にする。
    
    NG --genta_opt=some_flag
    OK --genta_opt=some_flag=true

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ generate-example:
 	protoc --proto_path=pb --go_out=out pb/*.proto
 
 generate-example2:
-	protoc --proto_path=pb --plugin=./protoc-gen-genta --genta_out=out pb/*.proto
+	protoc --proto_path=pb --plugin=./protoc-gen-genta --genta_opt=templates_path=templates --genta_out=out pb/*.proto
 
 build:
 	go build github.com/teach310/genta/cmd/protoc-gen-genta
---
+++ protogen/options.go
@@ -0,0 +1,27 @@
+package protogen
+
+import "strings"
+
+type Options struct {
+	TemplatesPath string
+}
+
+// paramter CodeGeneratorRequest.GetParameterで取得できるカンマ区切りのパラメータ
+func parseOptions(parameter string) *Options {
+	options := &Options{}
+	for _, param := range strings.Split(parameter, ",") {
+		i := strings.Index(param, "=")
+		if i < 0 {
+			continue // bool値でも必ずtrue, falseを明示的に指定することを必須とする。
+		}
+
+		key := param[0:i]
+		value := param[i+1:]
+
+		switch key {
+		case "templates_path":
+			options.TemplatesPath = value
+		}
+	}
+	return options
+}
